a7dae4b1711630364255e40d64dce4e95ba1eac3
Mathieu Duponchelle <mathieu@centricular.com>
2025-03-27 20:11:59 +0100

    awstranscriber2: fix potential underflow on discont

--- net/aws/src/transcriber2/imp.rs
+++ net/aws/src/transcriber2/imp.rs
@@ -229,7 +229,7 @@ impl Transcriber {
                                     .to_running_time(pts)
                                     .unwrap();
 
-                                state.discont_accumulator += base_rtime - discont_rtime;
+                                state.discont_accumulator += base_rtime.saturating_sub(discont_rtime);
 
                                 gst::info!(
                                     CAT,
